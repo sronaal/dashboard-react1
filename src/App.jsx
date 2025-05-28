fc1519fb9af87a6be7819b51d44719ff1f5b55b3
Ronal <ronal.sanx21@gmail.com>
2025-05-28 10:18:17 -0500

    Creacion Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+function App() {
+
+  return (
+    <>
+        <div className='text-gray-300'>
+          <BrowserRouter>
+            <Routes>
+            
+            </Routes>
+          </BrowserRouter>
+        </div>
+    </>
+  )
+}
+
+export default App
